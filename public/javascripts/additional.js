87774c58876ce42e7c86b9155f90723dbfc6d542
skubiak <sven.kubiak@maredit.de>
2013-05-03 15:04:28 +0200

    refactored javascript handliong, added logo

---
+++ public/javascripts/additional.js
@@ -0,0 +1,122 @@
+$('#ajaxplayday').hide();
+$('#ajaxloader').hide();
+$('#ajaxusers').hide();
+
+function helpify() {
+	$('.btn-primary').click(function() {
+		var btn = $(this)
+		btn.button('loading')
+	});
+	$('img').on().tooltip();
+	$('.admintooltip').on().tooltip();
+}
+
+function showCredits() {
+	$('#modalCredits').modal('show');
+}
+
+$(document).on('DOMNodeInserted', function(e) {
+	helpify();
+});
+
+$(document).ready(function(){
+	$('.alert-info').delay(4000).slideToggle();
+	$('.alert-success').delay(4000).slideToggle();
+	helpify();
+	window.addEventListener("load", function() {
+		setTimeout(function() {
+			window.scrollTo(0, 1);
+		}, 0);
+	});
+	if ($('.usermanagement').length != 0) {
+	    $('.usermanagement').dataTable( {
+	    	"bPaginate": false,
+	    	"bInfo": false,
+	    	"aaSorting": [[1,'asc']],
+	    	"aoColumnDefs": [ { "bSortable": false, "aTargets": [ 0,4,5,6 ] } ],
+	    	"oLanguage": {
+	            "sSearch": "<i class=\"icon-search\"></i>"
+	        }
+	    } );		
+	}
+	if ($('.standings').length != 0) {
+	    $('.standings').dataTable( {
+	    	"bPaginate": false,
+	    	"bInfo": false,
+	    	"aaSorting": [[0,'asc']],
+	    	"aoColumnDefs": [ { "bSortable": false, "aTargets": [ 1 ] } ],
+	    	"oLanguage": {
+	            "sSearch": "<i class=\"icon-search\"></i>"
+	        }
+	    } );		
+	}
+    $('#fat-btn-loading').click(function() { 
+    	$.blockUI({ message: '<h2>Calculating points...<br/>This may take a moment!<h2><img src="/public/img/ajax-loader.gif" />' });
+        setTimeout($.unblockUI, 60000); 
+    }); 
+});
+
+$('.navbar a, .subnav a').smoothScroll();
+
+(function ($) {
+	$(function(){
+		var $win = $(window),
+				$body = $('body'),
+				$nav = $('.subnav'),
+				navHeight = $('.navbar').first().height(),
+				subnavHeight = $('.subnav').first().height(),
+				subnavTop = $('.subnav').length && $('.subnav').offset().top - navHeight,
+				marginTop = parseInt($body.css('margin-top'), 10);
+				isFixed = 0;
+
+		processScroll();
+		$win.on('scroll', processScroll);
+		function processScroll() {
+			var i, scrollTop = $win.scrollTop();
+			if (scrollTop >= subnavTop && !isFixed) {
+				isFixed = 1;
+				$nav.addClass('subnav-fixed');
+				$body.css('margin-top', marginTop + subnavHeight + 'px');
+			} else if (scrollTop <= subnavTop && isFixed) {
+				isFixed = 0;
+				$nav.removeClass('subnav-fixed');
+				$body.css('margin-top', marginTop + 'px');
+			}
+		}
+	});
+})(window.jQuery);
+
+$(window).data('ajaxready', true).scroll(function(e) {
+	if ($(window).data('ajaxready') == false) return;
+    if ($(window).scrollTop() >= ($(document).height() - $(window).height())) {
+        $(window).data('ajaxready', false);
+        var start =	$('#lazyTable tr').length;
+        var users = $('#ajaxusers').text();
+        
+        if (start < users) {
+        	$('#ajaxloader').show();
+        	var playday = $('#ajaxplayday').text();
+            $.ajax({
+                cache: false,
+                url: '/overview/playday/' + playday + '/' + start,
+                success: function(html) {
+                    if (html) {
+                        $('#lazyTable tr:last').after(html);
+                    }
+                    $(window).data('ajaxready', true);
+                }
+            });
+            $('#ajaxloader').hide();
+        }
+    }
+});
+
+var url = $.url(); 
+var tab = url.attr('fragment');
+if (tab) {
+    $('ul.nav-tabs').children().removeClass('active');
+    $('a[href=#'+ tab +']').parents('li:first').addClass('active');
+    $('div.tab-content').children().removeClass('active');
+    $('#' + tab).addClass('active');
+    $('#' + tab).show();
+}
