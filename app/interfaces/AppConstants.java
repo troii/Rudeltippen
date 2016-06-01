2d7b6c88e6f334fe843e35aaf9c10586fe33781c
Thomas Einwaller <thomas.einwaller@troii.com>
2016-06-01 23:41:50 +0200

    first parsable version of em2016.yml

--- app/interfaces/AppConstants.java
+++ app/interfaces/AppConstants.java
@@ -1,17 +1,17 @@
 package interfaces;
 
 public interface AppConstants {
-	public static final String APPNAME = "rudeltippen";
-	public static final String YAMLFILE = "wm2014.yml";
-	public static final String DEFAULT_DATEFORMAT = "dd.MM.yyyy";
-	public static final String DEFAULT_TIMEFORMAT = "kk:mm";
-	public static final String DEFAULT_TIMEZONE = "Europe/Berlin";
-	public static final String WS_ENCODING = "UTF-8";
-	public static final String WS_CONTENT_TYPE = "application/soap+xml";
-	public static final String WS_URL = "http://www.openligadb.de/Webservices/Sportsdata.asmx";
-	public static final String CONFIRMATIONPATTERN = "\\w{8,8}-\\w{4,4}-\\w{4,4}-\\w{4,4}-\\w{12,12}";
-	public static final String EMAILPATTERN = ".+@.+\\.[a-z]+";
-	public static final String USERNAMEPATTERN = "[a-zA-Z0-9-_]+";
-	public static final int PICTURESMALL = 64;
-	public static final int PICTURELARGE = 128;
+	String APPNAME = "rudeltippen";
+	String YAMLFILE = "em2016.yml";
+	String DEFAULT_DATEFORMAT = "dd.MM.yyyy";
+	String DEFAULT_TIMEFORMAT = "kk:mm";
+	String DEFAULT_TIMEZONE = "Europe/Berlin";
+	String WS_ENCODING = "UTF-8";
+	String WS_CONTENT_TYPE = "application/soap+xml";
+	String WS_URL = "http://www.openligadb.de/Webservices/Sportsdata.asmx";
+	String CONFIRMATIONPATTERN = "\\w{8,8}-\\w{4,4}-\\w{4,4}-\\w{4,4}-\\w{12,12}";
+	String EMAILPATTERN = ".+@.+\\.[a-z]+";
+	String USERNAMEPATTERN = "[a-zA-Z0-9-_]+";
+	int PICTURESMALL = 64;
+	int PICTURELARGE = 128;
 }
